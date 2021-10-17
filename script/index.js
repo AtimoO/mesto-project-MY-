d4d45fb921dc71b7aab864b21796ae5020fa4d90
A. Timokhin <timoha.job@gmail.com>
2021-10-17 12:57:39 +0300

    update function btn add and edit

--- script/index.js
+++ script/index.js
@@ -1,34 +1,22 @@
-// open/close (#1)
-const popupElement = document.querySelector('.popup'); // for btn close popup
-const popupBtnClose = popupElement.querySelector('.popup__btn-close');
-const formElement = popupElement.querySelector('.popup__form');
-const nameInput = formElement.querySelector('#name');
-const jobInput = formElement.querySelector('#job');
+// popup Edit
+const popupEditElement = document.querySelector('.popup_edit');
+const popupEditBtnClose = popupEditElement.querySelector('.popup__btn-close'); // close Edit
+const formEditElement = popupEditElement.querySelector('.popup__form');
+const nameInput = formEditElement.querySelector('#name');
+const jobInput = formEditElement.querySelector('#job');
 
+// popup Add
+const popupAddElement = document.querySelector('.popup_add');
+const popupAddBtnClose = popupAddElement.querySelector('.popup__btn-close'); // close Edit
+
+// profile
 const profileElement = document.querySelector('.profile'); // for btn edit and add
-const profileBtnEdit = profileElement.querySelector('.profile__edit');
+const profileBtnEdit = profileElement.querySelector('.profile__edit'); // edit
+const profileBtnAdd = profileElement.querySelector('.profile__add'); // add
 
-function closePopup() {
-  popupElement.classList.remove('popup_opened');
-}
-
-profileBtnEdit.addEventListener('click', function () {
-  popupElement.classList.add('popup_opened');
-});
-
-popupBtnClose.addEventListener('click', closePopup);
-
-function formSubmitHandler(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы
-  profileElement.querySelector('.profile__title').textContent = nameInput.value;
-  profileElement.querySelector('.profile__subtitle').textContent = jobInput.value;
-  closePopup();
-}
-
-formElement.addEventListener('submit', formSubmitHandler); // Прикрепляем обработчик к форме
-
-
-// create Cards (#2)
+// array Cards #2
+const placesElement = document.querySelector('.places');
+const cardsContainer = placesElement.querySelector('.places__items');
 const initialCards = [
   {
     name: 'Архыз',
@@ -56,16 +44,41 @@ const initialCards = [
   }
 ];
 
-const container = document.querySelector('.places');
-const cardsContainer = container.querySelector('.places__items');
+// function opem/close #1
+function openPopup(popupElement) {
+  popupElement.classList.add('popup_opened');
+}
 
-initialCards.forEach(item => {
-  // тут можно будет вызвать функцию добавления карт
-  addCard(item.name, item.link);
+function closePopup(popupElement) {
+  popupElement.classList.remove('popup_opened');
+}
+
+// open/close modal edit #1 #3
+profileBtnEdit.addEventListener('click', function () {
+  openPopup(popupEditElement);
+});
+profileBtnAdd.addEventListener('click', function () {
+  openPopup(popupAddElement);
 });
 
+popupEditBtnClose.addEventListener('click', function () {
+  closePopup(popupEditElement);
+});
+popupAddBtnClose.addEventListener('click', function () {
+  closePopup(popupAddElement);
+});
 
+// btn save Edit
+function formSubmitHandler(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы
+  profileElement.querySelector('.profile__title').textContent = nameInput.value;
+  profileElement.querySelector('.profile__subtitle').textContent = jobInput.value;
+  closePopup(popupEditElement);
+}
 
+formEditElement.addEventListener('submit', formSubmitHandler); // Прикрепляем обработчик к форме
+
+//
 function addCard (name, link) {
   const placesTemplate = document.querySelector('#places-template').content; // получаем содержимое template
   const placesElement = placesTemplate.querySelector('.places__item').cloneNode(true); // копируем содержимое шаблона
@@ -75,8 +88,20 @@ function addCard (name, link) {
   placesElement.querySelector('.places__btn-like').addEventListener('click', function(evt) { // добавляем обработчик событий для клика
     evt.target.classList.toggle('places__btn-like_active'); // если такого класса нет, то добавляем, если есть удаляем
   });
+  placesElement.querySelector('.places__image').addEventListener('click', function(evt) { // для открытия картинок
+    console.log('open img');
+  });
+
   cardsContainer.append(placesElement); // добавляем в конец контейнера songsContainer
 };
+
+// create initial cards #2
+initialCards.forEach(item => {
+  addCard(item.name, item.link);
+});
+
+
+
 
 
 
