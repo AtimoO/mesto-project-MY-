c73867fb93f8cd8eaf4f5cca57ec6640186fa660
A. Timokhin <timoha.job@gmail.com>
2021-10-20 19:28:33 +0300

    upd function addPlaces and create new function render

--- script/index.js
+++ script/index.js
@@ -4,27 +4,28 @@ const popupEditBtnClose = popupEditElement.querySelector('.popup__btn-close');
 const formEditElement = popupEditElement.querySelector('.popup__form');
 const nameInput = formEditElement.querySelector('#name');
 const jobInput = formEditElement.querySelector('#job');
-
 // popup Add
 const popupAddElement = document.querySelector('.popup_add');
 const popupAddBtnClose = popupAddElement.querySelector('.popup__btn-close');
 const formAddElement = popupAddElement.querySelector('.popup__form');
 const nameCardInput = formAddElement.querySelector('#name-card');
 const linkInput = formAddElement.querySelector('#link');
-
 // popup View-image
 const popupViewImageElement = document.querySelector('.popup_view-image');
+const popupImage = popupViewImageElement.querySelector('.popup__image');
+const popupImageTitle = popupViewImageElement.querySelector('.popup__text')
 const popupViewImageBtnClose = popupViewImageElement.querySelector('.popup__btn-close')
-
 // profile
-const profileElement = document.querySelector('.profile'); // for btn edit and add
+const profileElement = document.querySelector('.profile');
+const profileTitle = profileElement.querySelector('.profile__title');
+const profileSubtitle = profileElement.querySelector('.profile__subtitle');
 const profileBtnEdit = profileElement.querySelector('.profile__edit');
 const profileBtnAdd = profileElement.querySelector('.profile__add');
 // places
 const placesElement = document.querySelector('.places');
+const placesTemplate = document.querySelector('#places-template').content; // get template
 const cardsContainer = placesElement.querySelector('.places__items');
 
-// open/close popup
 function openPopup(popupElement) {
   popupElement.classList.add('popup_opened');
 }
@@ -33,13 +34,58 @@ function closePopup(popupElement) {
   popupElement.classList.remove('popup_opened');
 }
 
+function likeBtn (evt) {
+  evt.target.classList.toggle('places__btn-like_active');
+}
+
+function addPlaces(name, link) {
+  const placesItemElement = placesTemplate.querySelector('.places__item').cloneNode(true); // copy template
+  placesItemElement.querySelector('.places__title').textContent = name;
+  const placesImage = placesItemElement.querySelector('.places__image');
+  placesImage.src = link;
+  placesImage.alt = name;
+  // likeBtn
+  placesItemElement.querySelector('.places__btn-like').addEventListener('click', likeBtn);
+  // popup view img
+  placesItemElement.querySelector('.places__image').addEventListener('click', function (evt) {
+    popupImageTitle.textContent = name;
+    popupImage.src = link;
+    popupImage.alt = name;
+    openPopup(popupViewImageElement);
+  });
+  // Удаление карточки
+  placesItemElement.querySelector('.places__btn-remove').addEventListener('click', function (evt) {
+    evt.target.parentElement.remove();
+  });
+  return placesItemElement;
+};
+
+function renderCard(name, link) {
+  cardsContainer.prepend(addPlaces(name, link));
+}
+
+function handlerEditFormSubmit(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+  closePopup(popupEditElement);
+}
+
+function handlerAddFormSubmit(evt) {
+  evt.preventDefault();
+  renderCard(nameCardInput.value, linkInput.value)
+  closePopup(popupAddElement);
+  nameCardInput.value = '';
+  linkInput.value = '';
+}
+
+// open/close popup
 profileBtnEdit.addEventListener('click', function () {
   openPopup(popupEditElement);
 });
 profileBtnAdd.addEventListener('click', function () {
   openPopup(popupAddElement);
 });
-
 popupEditBtnClose.addEventListener('click', function () {
   closePopup(popupEditElement);
 });
@@ -49,54 +95,11 @@ popupAddBtnClose.addEventListener('click', function () {
 popupViewImageBtnClose.addEventListener('click', function () {
   closePopup(popupViewImageElement);
 });
-
 // saveBtn popupEdit
-function formEditSubmitHandler(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы
-  profileElement.querySelector('.profile__title').textContent = nameInput.value;
-  profileElement.querySelector('.profile__subtitle').textContent = jobInput.value;
-  closePopup(popupEditElement);
-}
-
-formEditElement.addEventListener('submit', formEditSubmitHandler); // Прикрепляем обработчик к форме
-
-function addPlaces(name, link) {
-  const placesTemplate = document.querySelector('#places-template').content; // получаем содержимое template
-  const placesElement = placesTemplate.querySelector('.places__item').cloneNode(true); // копируем содержимое шаблона
-  placesElement.querySelector('.places__title').textContent = name;
-  placesElement.querySelector('.places__image').src = link;
-  placesElement.querySelector('.places__image').alt = name;
-
-  placesElement.querySelector('.places__btn-like').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('places__btn-like_active');
-  });
-  // popup view img
-  placesElement.querySelector('.places__image').addEventListener('click', function (evt) {
-    const popupImage = popupViewImageElement.querySelector('.popup__image');
-    popupViewImageElement.querySelector('.popup__text').textContent = name;
-    popupImage.src = link;
-    popupImage.alt = name;
-    openPopup(popupViewImageElement);
-  });
-  // Удаление карточки
-  placesElement.querySelector('.places__btn-remove').addEventListener('click', function (evt) {
-    evt.target.parentElement.remove();
-  });
-  cardsContainer.prepend(placesElement); // добавляем в начало контейнера
-};
-
+formEditElement.addEventListener('submit', handlerEditFormSubmit); // Прикрепляем обработчик к форме
+// createBtn popupAdd
+formAddElement.addEventListener('submit', handlerAddFormSubmit);
 // create initial cards
 initialPlaces.forEach(item => {
-  addPlaces(item.name, item.link);
+  renderCard(item.name, item.link);
 });
-
-// createBtn popupAdd
-function formAddSubmitHandler(evt) {
-  evt.preventDefault();
-  addPlaces(nameCardInput.value, linkInput.value);
-  closePopup(popupAddElement);
-  nameCardInput.value = '';
-  linkInput.value = '';
-}
-
-formAddElement.addEventListener('submit', formAddSubmitHandler);
