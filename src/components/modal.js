50da8d4207991530d12a4411af5772e4d80f3ba2
A. Timokhin <timoha.job@gmail.com>
2021-12-23 18:39:16 +0300

    feat: setting webpack

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,6 +1,6 @@
 import { closePopup } from "./utils.js";
 import { popupViewImageElement } from "./card.js";
-import { popupEditElement, popupAddElement } from "./index.js";
+import { popupEditElement, popupAddElement } from "../pages/index.js";
 
 function closePopupInOverlay(evt) {
   if (evt.target.classList.contains("popup")) {
